80c9e275dc61377bfd5bbab911d9f6542813e51b
alsoj <alsoj@naver.com>
2022-08-25 22:06:34 +0900

    create cacheProxy

--- src/test/java/hello/proxy/pureproxy/proxy/ProxyPatterTest.java
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatterTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatterClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,17 @@ public class ProxyPatterTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatterClient client = new ProxyPatterClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatterClient client = new ProxyPatterClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
